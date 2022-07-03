bddb9ab355508db7c330c94f2db975ed6015f449
andropyro28 <menandroeugenio1028@gmail.com>
2022-07-03 04:59:20 -0400

    Global styles with bootstrap

---
+++ pages/about.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+  return (
+    <h2>
+      About page 
+      <button className='btn btn-success'>Primary</button>
+    </h2>
+  )
+}
+
+export default About
