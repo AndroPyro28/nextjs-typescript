904848e6f4e54ba3a9d975064a158ce10276e0bc
andropyro28 <menandroeugenio1028@gmail.com>
2022-07-06 02:57:24 -0400

    ENV

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,19 @@ import React from 'react'
 
 function index() {
   return (
-    <div>index</div>
+    <div>{process.env.NEXT_PUBLIC_ANALYTICS_ID}</div>
   )
 }
 
 export default index
+
+export const getServerSideProps = () => {
+  const user = process.env.DB_USER;
+  const passsword = process.env.DB_PASSWORD;
+  console.log(user, passsword);
+  return {
+    props: {
+      key: "value"
+    }
+  }
+}
